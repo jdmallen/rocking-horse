ad8e3e230840168e0a25d86476bbb78580aaded3
J.D. Mallen <jessedmallen@gmail.com>
2025-01-19 11:19:49 -0500

    Install modules, base styles, colors

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/devtools',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    'nuxt-security',
+    'nuxt-gtag'
+  ],
+  css: [
+    'modern-normalize/modern-normalize.css'
+  ]
 })
---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>Home Page</h1>
+		<p>
+			<a>Lorem ipsum dolor sit amet,</a> ipsum ut deserunt qui Lorem dolor nulla pariatur off
+icia Lorem do aliquip nisi aute voluptate anim minim dolor sit exercitation. Ut 
+magna adipisicing ut esse et do veniam dolor amet incididunt culpa commodo. Eius
+mod quis voluptate elit occaecat sint amet consequat minim veniam tempor labore 
+sunt sit commodo. <strong>Ad nostrud sit reprehenderit</strong> eu sint ad ea elit enim Lorem min
+im exercitation. Amet cillum magna do pariatur dolore elit magna aliquip cupidat
+at excepteur. Laborum veniam amet ut duis proident aute adipisicing nulla. Conse
+ctetur amet mollit adipisicing sunt aliqua ea.
+
+		</p>
+		<button>button</button>
+	</div>
+</template>
