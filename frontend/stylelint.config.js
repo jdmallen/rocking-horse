71104ef3146add6f409dcf7b59f8b8b0a779ce35
J.D. Mallen <jessedmallen@gmail.com>
2025-01-19 15:50:33 -0500

    Actually fix linting, since Nuxt wasn't starting

--- frontend/stylelint.config.js
+++ frontend/stylelint.config.js
@@ -1,6 +1,9 @@
 /** @type {import('stylelint').Config} */
 export default {
-	files: ["**/*.{css,scss,vue}"],
+	files: ["**/*.css"],
 	plugins: ["stylelint-order"],
-	extends: ["stylelint-config-standard"],
+	extends: ["stylelint-config-standard-vue"],
+	rules: {
+		"custom-property-empty-line-before": null,
+	},
 };
