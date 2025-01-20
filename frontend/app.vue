a321b6608925e35bf58ac48ef5947b4b3af4fcfc
J.D. Mallen <jessedmallen@gmail.com>
2025-01-19 20:51:10 -0500

    Add meta, more light/dark, hover effects

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,16 @@
-<script lang="js">
-	export const App = {
-		mounted() {},
-	};
+<script lang="ts">
+	useHead({
+		title: "Rocking Horse Produtions",
+		meta: [
+			{
+				name: "description",
+				content:
+					"Welcome to Rocking Horse Productions, your premier community theater in Williamsville, NY. Discover our shows, events, and more!",
+			},
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	});
 </script>
 
 <template>
